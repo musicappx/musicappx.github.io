c728c8d0976644dd06ccf6d58c1ca263c4bc6463
musicapp <musicapp@163.com>
2018-08-24 22:59:51 +0800

    update

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>http://musicappblog.com</homePageLink>
+    <homePageLink>https://www.musicapp.ml</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://musicappblog.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://musicappblog.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://musicappblog.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://musicappblog.com/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://musicappblog.com/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.musicapp.ml/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.musicapp.ml/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.musicapp.ml/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.musicapp.ml/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.musicapp.ml/wp-json/" />
 	    </apis>
   </service>
 </rsd>
